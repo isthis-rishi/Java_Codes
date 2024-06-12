6ccde4ab3d7a73bc7b04afe02875d86cf0c43cc7
Rishi Kumar <125174773+isthis-rishi@users.noreply.github.com>
2024-06-12 08:16:59 +0530

    Update allSubArray.java

--- Java Codes/allSubArray.java
+++ Java Codes/allSubArray.java
@@ -8,7 +8,7 @@ Write your code in this editor and press "Run" button to execute it.
 
 public class allSubArray
 {
-    public static void allSubArray(int arr[]){
+    public static void printAllSubArrays(int arr[]){
         for (int i=0; i<arr.length ; i++ ){
             for (int j =i; j < arr.length ; j++ ){
                 for (int k = i; k<=j ; k++ ){
@@ -23,6 +23,6 @@ public class allSubArray
 	public static void main(String[] args) {
 		System.out.println("Hello World");
 		int arr[] = {1,2,3,4,5,6};
-		allSubArray(arr);
+		printAllSubArrays(arr);
 	}
 }
